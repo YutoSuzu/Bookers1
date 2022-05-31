4469820a96c1778511fbdd512fafe718a9491cd3
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-05-31 10:01:40 +0000

    [add]edit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+
+  end
+
+  def create
+    flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to '/books'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    flash[:notice] = "Book was successfully update."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root :to => "homes#top"
+
+  get 'homes/top'
+
+  resources :books
+  
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
